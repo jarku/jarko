9694b571e11e1716f31d48eb78d7765e51f14ae3
jarku <desirq@gmail.com>
2017-02-13 22:38:47 +0100

    new background, completed sections

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,24 @@
-/**
- * Created by jarko on 26.01.2017.
- */
+var current_item = 0;
+
+jQuery(document).ready(function($) {
+  $("a", '.mainMenu').click(function()
+  {
+    if( ! $(this).hasClass('active') ) {
+      current_item = this;
+
+      $('.section:visible').fadeOut( 1000, function() {
+        $('a', '.mainMenu').removeClass( 'active' );
+        $(current_item).addClass( 'active' );
+        var new_section = $( $(current_item).attr('href') );
+        new_section.fadeIn( 1000 );
+      } );
+    }
+    $('.navbar-collapse-fix').collapse('toggle');
+    $('.nav-icon3').removeClass('open');
+    return false;
+  });
+
+  $('.nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+});
